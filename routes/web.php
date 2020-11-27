4be0b13440feacc3beb8eb440eb10bc9d67bb5ed
smileyuichi <smile.yuichi.1013@gmail.com>
2020-11-27 11:32:08 +0900

    laravel learn start

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('hello/{msg}',function($msg){
+    $html = <<<EOF
+<html>
+<head>
+<title>Hello</title>
+<style>
+body {font-size:16px; color:#999;}
+h1 { font-size:100px; text-align:right; color:#eee; margin:-40px 0 -50px 0; }
+</style>
+</head>
+<body>
+<h1>Hello</h1>
+<p>{$msg}</p>
+<p>これはサンプルで作ったページ</p>
+</body>
+</html>
+EOF;
+    return $html;
+});
